f28b974723fabbf04f6d0237834e2f0a98b62211
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2024-05-16 16:01:19 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+    $hostname = "localhost";
+    $userDataBase = "root";
+    $passwordUser = "";
+    $databaseName = "recipesave";
+
+    $connection = mysqli_connect($hostname, $userDataBase, $passwordUser, $databaseName) or die(mysqli_error());
+
+    //if ($connection){
+    //    echo "Berhasil Koneksi";
+    //} else echo "Gagal koneksi";
+?>
