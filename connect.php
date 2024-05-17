d25fc28b207509f31d1de140fb13b5534cdf2a32
loveg0od <rivandi29@gmail.com>
2024-05-17 21:51:48 +0700

    ehe

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $hostname = "localhost";
     $userDataBase = "root";
     $passwordUser = "";
-    $databaseName = "recipesave";
+    $databaseName = "masakuy";
 
     $connection = mysqli_connect($hostname, $userDataBase, $passwordUser, $databaseName) or die(mysqli_error());
 
