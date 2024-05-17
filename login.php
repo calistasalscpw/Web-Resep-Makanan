9554f38ca294112b552d5b879c872d0f839e1e5d
loveg0od <142822169+loveg0od@users.noreply.github.com>
2024-05-17 19:59:15 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,42 +1,6 @@
 <?php
 
-session_start();
-require 'database.php';
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM user WHERE email = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    //memeriksa apakah pengguna ada
-    if($result->num_rows > 0){
-        $user = $result -> fetch_assoc();
-        //verifikasi kata sandi
-        if(password_verify($password, $user['password_hash'])){
-            //simpan informasi pengguna dalam sesi
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['name'] = $user['name'];
-            //arahkan ke halaman index
-            header("Location: index.php");
-            exit;
-        } else{
-            echo "Kata sandi yang anda masukkan salah";
-        }
-    } else {
-        echo "Pengguna tidak ditemukan";
-    }
-
-    $stmt->close();
-}
-
-$conn->close();
-
-/*$is_invalid = false;
+$is_invalid = false;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     
@@ -66,13 +30,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
     
     $is_invalid = true;
-}*/
+}
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Masak.Uy</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -82,25 +46,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-<body style="font-family: Poppins, sans-serif;">
-    
-    
-    <?php if ($is_invalid): ?>
-        <em>Invalid login</em>
-    <?php endif; ?>
-    
-    
 
+<?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+<?php endif; ?>
+
+<body style="font-family: Poppins, sans-serif;">    
 <div class="card">
-    <div class="card text-center d-flex flex-column align-items-center" style="margin-top: 1em;"><img class="mb-4" src="assets/img/illustrations/iconColor.png" style="max-height: 200px;max-width: 250px;margin-top: 60px;">
+    <div class="card-body text-center d-flex flex-column align-items-center">
+    <img class="mb-4" src="assets/img/illustrations/iconColor.png" style="max-height: 200px;max-width: 250px;margin-top: 60px; color: #f6ae48;"> 
         <div class="card mb-3" style="max-width: 650px; border-radius: 1em; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.35);">
             <div class="row g-0">
-              <div class="col-md-8" style="background-color: #f6ae48; color: #ffffff; width: 20em; height: 30em; border-radius: 1em; z-index: 9;">
-                <p style="font-size: 1.5em; font-weight: bold; margin-top: 2em; margin-bottom: 0;">Hi! Good To See You</p>
-                <p style="font-size: 0.8em;">Time To Make A Delicious Foodd</p>
-                <form  class="d-flex flex-column" method="post" data-bs-theme="light" style="width: 300px;max-width: none; margin: auto;">
+                <div class="col-md-8" style="background-color: #f6ae48; color: #ffffff; width: 20em; height: 30em; border-radius: 1em; z-index: 9;">
+                    <p style="font-size: 1.5em; font-weight: bold; margin-top: 2em; margin-bottom: 0;">Hi! Good To See You</p>
+                    <p style="font-size: 0.8em;">Time To Make A Delicious Foodd</p>
+                    <form  class="d-flex flex-column" method="post" data-bs-theme="light" style="width: 300px;max-width: none; margin: auto;">
                     <div class="mb-3"><input class="form-control" type="text" name="email" placeholder="Email" style="background-color: #E29D3D; border: none; margin-top: 2em; color: #fff;"  value="<?= htmlspecialchars($_POST["email"] ?? "") ?>"></div>
                     <div class="mb-4"><input class="form-control" type="password" name="password" placeholder="Password"style="background-color: #E29D3D; border: none; color: #fff" ></div>
                     <div class="input-group mb-3">
@@ -113,13 +75,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <div class="mb-4"><button class="btn shadow d-block w-100" type="submit" style="color: #ffffff;background: #FB6D48; font-weight: 800;">Login</button></div>
                     <p style="font-size: 0.8em;">Don't have any account? Sign Up <a href="signup.html" style=" font: size 0.8em; text-decoration: none; font-weight: 600; color: #ffffff;">here</a></p>
                   </form>
-              </div>
-              <div class="col" style="position: relative; right: 1em; max-width: 100%;">
+                </div>
+                <div class="col" style="position: relative; right: 1em; max-width: 100%;">
                 <img src="assets/img/food.png" class="img-fluid food-image" style="height: 30em; z-index: 8;">
-              </div>
+                </div>
             </div>
-          </div>
-
+        </div>
     </div>
 </div>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
